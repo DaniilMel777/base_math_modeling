497468604e6e66785dcad0ec54ece81e37256aa9
DaniilMel777 <meldaniil53@gmail.com>
2021-03-27 10:55:58 +0000

    pam pam pam

--- less1.py
+++ less1.py
@@ -1,2 +1,29 @@
-print('skybidyuiyiuybidy')
-print('i smart')
+s=3+4
+print(s)
+
+s=5
+print(s)
+
+type(3)
+print(type(3))
+
+print(type(3.4))
+
+print(type('красава'))
+
+
+g=['krasava', 1, 'kryto', 6, 8]
+print(type(a))
+
+print(type(True))
+print(type(False))
+
+a=int(input('Введите значение a:'))
+print(type(g))
+
+a=[1, 3, 6]
+print(a[0])
+
+
+
+
---
+++ t.py
@@ -0,0 +1,13 @@
+a=[1, 3, 6]
+b=[2, 4, 7]
+c=a+b
+print(c)
+
+s=[]
+s.append('good')
+print(s)
+
+a.append(b)
+print(c)
+
+print(len(a))
